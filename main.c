effbbd53c3f59eb0c1fddad1e542b1461b8a0c26
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-10-12 17:00:22 +0900

    05

--- main.c
+++ main.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a){
-    return (a*a);
+int factorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
 }
 
-int main(){
-    int a = 2;
-    a = square(a);
-    printf("a=%i\n", a);
+int combination(int n, int r) {
+    int numerator = factorial(n);
+    int denominator = factorial(n-r)*factorial(r);
     
-    system("PAUSE");	
-    return 0;
+    return (numerator/denominator);
+} 
+
+int get_integer(){
+    int value;
+    printf("Enter an integer:");
+    scanf("%d", &value);
+    return value;
+}
+
+int main(void){
+    int n, r;
+    int result;
+    
+    printf("Input n:");
+    scanf("%d", &n);
+    //n = get_integer(); 
+    
+    printf("Input r: ");
+    scanf("%d", &r);
+    //r = get_integer();
+    
+   result = combination(n, r);
+   
+   printf("C(%d, %d) = %d\n", n, r, result); 
+   system("PAUSE");	
+   return 0;
 }
 
